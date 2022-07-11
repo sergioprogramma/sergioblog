ec47e17807701c101c85a437b30cd96f817f61d4
sergiodeveloper <104594659+sergioprogramma@users.noreply.github.com>
2022-07-11 15:37:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import os
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.create_all()
