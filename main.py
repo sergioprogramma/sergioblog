7d821d5fa6d265a20032b19f42536dee66f62d5f
sergioburigana <buriganasergio@gmail.com>
2022-07-12 12:54:27 -0400

    blog complete

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 app = Flask(__name__)
 
 # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATA_BASE1')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATA_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.create_all()
